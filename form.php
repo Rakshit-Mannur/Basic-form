fb2bd798809dbf44af985940176b893d5bea3fae
Rakshit Mannur <rakshitmannur00@gmail.com>
2021-05-25 15:50:45 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <title>Form</title>
+</head>
+<body>
+<form action="index.php" method="POST" enctype="multipart/form-data">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Firstname</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" name="firstname" id="firstname">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Lastname</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" name="lastname" id="lastname">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Branch</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" name="branch" id="branch">
+  </div>
+  <input type="submit" class="btn btn-primary" value="Submit" id="submit" name="submit"></input>
+</form>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First project</title>
+</head>
+
+<body>
+    <?php 
+        $servername='localhost';
+        $username='root';
+        $password='king';
+        $dbname='firstPro';
+
+        $conn=mysqli_connect($servername,$username,$password,$dbname);
+
+        if(!$conn)
+        {
+            die('Connection failed: '.mysqli_connect_error());
+        }
+
+        if(isset($_POST['submit']))
+        {
+         $firstname =  $_POST['firstname'];
+         $lastname = $_POST['lastname'];
+         $branch =  $_POST['branch'];
+
+         $sql="INSERT INTO list values('".$_POST["$firstname"]."','".$_POST["$lastname"]."','".$_POST["$branch"]."')";
+
+         if(mysqli_query($conn,$sql))
+         {
+            echo 'You are registered successfully !';
+         }
+         else
+         {
+            echo 'OOPS! Registration failed';
+         }
+
+         mysqli_close($conn);
+       }
+    ?>
+    
+</body>
+</html>
+
+
+
+
